37f1d1eafb97c4afa015cc3e40b3a70130146008
E.Bousk <e.bousk@gmail.com>
2021-07-31 15:21:19 +0200

    Images filenames are now displayed on input

--- assets/app.js
+++ assets/app.js
@@ -8,5 +8,10 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
+
+$(".custom-file-input").on('change', function(e) {
+    var inputFile= e.currentTarget;
+    $(inputFile).parent().find('.custom-file-label').html(inputFile.files[0].name);
+});
