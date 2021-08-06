195d84a1e11583b32229346ccaae47f27fbffa41
E.Bousk <e.bousk@gmail.com>
2021-08-06 20:25:35 +0200

    Display flash message (and redirect to homepage) on logout. Display flash message (and redirect to login page) on successfully reseted password. Rename variable

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -130,7 +130,9 @@ class ResetPasswordController extends AbstractController
             // The session is cleaned up after the password has been changed.
             $this->cleanSessionAfterReset();
 
-            return $this->redirectToRoute('home');
+            $this->addFlash('success', 'Your password has been successfully reseted. You can now log in with the new password.');
+
+            return $this->redirectToRoute('app_login');
         }
 
         return $this->render('reset_password/reset.html.twig', [
